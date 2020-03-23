36431cdadc6804ff49eb7890b218cb8d47286970
WeisonWei <weixx3@126.com>
2020-03-23 17:06:32 +0800

    init

--- src/main/java/com/es/user/jwt/ESchoolServiceUserOauth2JwtApplication.java
+++ src/main/java/com/es/user/jwt/ESchoolServiceUserOauth2JwtApplication.java
@@ -12,10 +12,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = {"com.es.user.jwt.repository"})
 @EnableJpaAuditing
 @EnableDiscoveryClient
-public class ESchoolUserServiceOauth2JwtApplication {
+public class ESchoolServiceUserOauth2JwtApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ESchoolUserServiceOauth2JwtApplication.class, args);
+        SpringApplication.run(ESchoolServiceUserOauth2JwtApplication.class, args);
     }
 
 }
--- src/test/java/com/es/user/jwt/ESchoolServiceUserOauth2JwtApplicationTests.java
+++ src/test/java/com/es/user/jwt/ESchoolServiceUserOauth2JwtApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ESchoolUserServiceOauth2JwtApplicationTests {
+class ESchoolServiceUserOauth2JwtApplicationTests {
 
     @Test
     void contextLoads() {
